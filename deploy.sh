47b669f77efff4370085b65ba5d6d98cafe8fcaa
wangjun <wangjun@hatech.com.cn>
2020-07-06 13:54:20 +0800

    添加自动化部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,18 @@
+###
+ # @Author: wangjun
+ # @LastEditors: wangjun
+ # @Date: 2020-07-06 13:16:03
+ # @LastEditTime: 2020-07-06 13:53:54
+ # @Description: 
+### 
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd public
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
